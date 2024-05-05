d6c257dadccd98dd649f148aeee258de34bb745d
Ahmad Santoso <ahmadsantoso800@gmail.com>
2024-05-05 21:10:03 +0700

    php form handling

---
+++ form_post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>FORM POST</title>
+</head>
+<body>
+    <form action="proses_post.php" method="POST">
+        Nama : <input type="text" name="nama">
+        Alamat : <input type="text" name="alamat">
+
+        <input type="submit" value="simpan">
+    </form>
+</body>
+</html>
